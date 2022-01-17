c7896f01f3de551cf14ae71c8d6610d5b5852060
Burak Altıntaş <burak.altintas@yahoo.com.tr>
2022-01-17 16:46:41 +0300

    first commit

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,30 @@
+import React, { useRef, useContext } from "react";
+import classes from "./NewTodo.module.css";
+import { TodosContext } from "../store/todos-context";
+
+const NewTodo: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    todosCtx.addTodo(enteredText);
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
---
+++ src/models/todo.ts
@@ -0,0 +1,13 @@
+// type || class || interface
+
+class Todo {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+export default Todo;
